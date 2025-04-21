09410a346a6bd7a7c0ea26ccb22abd879b6775b0
kangsuyeong <ekdh16@naver.com>
2025-04-21 12:25:03 +0900

    feat: day4까지 완료

---
+++ src/components/SearchableLayout.tsx
@@ -0,0 +1,47 @@
+import { ReactNode, useEffect, useState } from "react";
+import style from "./SearchableLayout.module.css";
+import { useRouter } from "next/router";
+
+export default function SearchableLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const router = useRouter();
+  const q = router.query.q as string;
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  return (
+    <div>
+      <div className={style.searchable_container}>
+        <input
+          value={search}
+          onChange={onChangeSearch}
+          placeholder="검색어를 입력하세요 ..."
+          onKeyDown={onKeyDown}
+        />
+        <button onClick={onSubmit}>검색</button>
+      </div>
+      {children}
+    </div>
+  );
+}
---
+++ src/pages/search.tsx
@@ -0,0 +1,13 @@
+import SearchableLayout from "@/components/SearchableLayout";
+import { useRouter } from "next/router";
+import { ReactNode } from "react";
+
+export default function Page() {
+  const router = useRouter();
+  const { q } = router.query;
+  return <h1>검색 결과 : {q}</h1>;
+}
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
